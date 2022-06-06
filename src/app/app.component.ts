2b1cb25a65110777a0c3e01b6d216386494773bf
sohaib047 <sohaibali9243@gmail.com>
2022-06-06 19:40:20 +0500

    Initial Push of all files

---
+++ src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//properties
+export class AppComponent {
+  title = 'World';
+  name = "Sohaib";
+  disable = false;
+  color = 'red'
+  bgColor = 'brown'
+
+  //style binding properties
+  updateColor(){
+    this.color = "yellow"
+    this.bgColor = "black"
+  }
+  //form
+  userData:any = {};
+
+  submit(data:NgForm){
+    console.log(data);
+    this.userData=data;
+  }
+
+  //user define code
+  intro = 'Sohaib Ali';
+  display_result = ''
+
+  Time() {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    return time
+  }
+  //making an array and using it through *ngFor
+  users = ['Sohaib', 'Ali', 'Kashmiri']
+  // userDetail = [
+  //   { name: 'Bilal', email: 'bilal@gmail.com', phone: '0900789601' },
+  //   { name: 'Akbar', email: 'akbar@gmail.com', phone: '0341789601' },
+  //   { name: 'Naseem', email: 'naseem@gmail.com', phone: '0213789601' },
+  // ]
+  persons = [
+    {nam:'Ali', email:'ali@outlook.com', socialAccounts : ['Facebook', 'Instagram', 'Snapchat']},
+    {nam:'Mehdi', email:'Mehdi@yahoo.com', socialAccounts : ['Facebook', 'Instagram', 'Snapchat']},
+    {nam:'Mohibullah', email:'Mohibullah@gmail.com', socialAccounts : ['Facebook', 'Instagram', 'Snapchat']},
+  ]
+
+
+
+  //Button Toggle
+  toDisplay = true;
+
+  unchanged() {
+    this.toDisplay = !this.toDisplay;
+  }
+
+  func_check() {
+    alert("function of button called")
+  }
+
+  display(name: string) {
+    alert("Text: " + name)
+  }
+
+  display_text(name: string) {
+    this.display_result = name
+  }
+
+  warner() {
+    alert("Beware of the Dog!")
+    console.warn("Beware of the Dog!")
+  }
+
+  get_input_data(name: string) {
+    console.log("The incoming value is: " + name)
+  }
+  //Making a counter
+  count = 0
+  counter(type: string) {
+    type === 'add' ? this.count++ : this.count--;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    UserAuthModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
