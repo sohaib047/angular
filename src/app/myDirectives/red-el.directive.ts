816355f42e66f8a22df2682bb6953b024f680cc1
sohaib047 <sohaibali9243@gmail.com>
2022-06-08 01:29:12 +0500

    learned about directives and made a custome one

--- src/app/myDirectives/red-el.directive.ts
+++ src/app/myDirectives/red-el.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appRedEl]'
 })
 export class RedElDirective {
 
-  constructor() { }
+  constructor(el:ElementRef) {
+    el.nativeElement.style.color="red"
+    el.nativeElement.innerText ="START: "  
 
+   }
+  
 }
